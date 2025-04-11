a94e3d06f85ea0e1af3091d19f2ef36dffcfdbfa
prabha55555 <k.prabhakaran.in@gmail.com>
2025-04-11 12:37:55 +0530

    Import the index

--- gen/src/main.jsx
+++ gen/src/main.jsx
@@ -4,6 +4,8 @@ import './index.css'
 import App from './App.jsx'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
+
 
 
 createRoot(document.getElementById('root')).render(
